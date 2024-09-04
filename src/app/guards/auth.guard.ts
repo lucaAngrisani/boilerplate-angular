d5a731c783b14eeb869447079850c8ccd45a64ca
Luca Angrisani <angrisani.luca@gmail.com>
2024-09-04 12:30:47 +0200

    fixes

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,13 +1,13 @@
 import { inject } from '@angular/core';
-import { Router, type CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, type CanActivateFn } from '@angular/router';
 import { SessionUserService } from '../services/session/session-user.service';
 import { ROUTE } from '../shared/route.enum';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const router: Router = inject(Router);
   const sessionUserService: SessionUserService = inject(SessionUserService);
 
-  if (sessionUserService.userObservable.getValue()) {
+  if (sessionUserService.userLogged()) {
     return true;
   } else {
     router.navigate([ROUTE.PUBLIC]);
--- src/app/services/session/local-storage.service.ts
+++ src/app/services/session/local-storage.service.ts
@@ -10,17 +10,34 @@ export class LocalStorageService {
 
   constructor() { }
 
-  /************************/
-  /* save item{key, obj}  */
-  /************************/
-  public save(key: string, object: any) {
+  /**
+   * Saves an object in sessionStorage with a specified key.
+   *
+   * This method serializes the provided object into a JSON string and stores it
+   * in sessionStorage under the given key.
+   *
+   * @param {string} key - The key under which the object will be stored in sessionStorage.
+   * @param {any} object - The object to be stored, which will be serialized into JSON.
+   *
+   * @returns {void} - No return value.
+   */
+  public save(key: string, object: any): void {
     localStorage.setItem(key, JSON.stringify(object));
   }
 
-  /************************/
-  /* load item by key     */
-  /************************/
-  public load(key: string) {
+  /**
+   * Loads and returns an object of type T from sessionStorage by the specified key.
+   *
+   * This method retrieves an item from sessionStorage, parses it from JSON,
+   * and returns the object as type T. If no item is found for the provided key, it returns `null`.
+   *
+   * @template T - The expected return type of the parsed object.
+   *
+   * @param {string} key - The key under which the object is stored in sessionStorage.
+   *
+   * @returns {T|null} - The parsed object of type T if found, or `null` if no item exists for the provided key.
+   */
+  public load<T>(key: string): T {
     this._obj = null;
     this._item = localStorage.getItem(key);
     if (this._item != null) {
@@ -29,10 +46,16 @@ export class LocalStorageService {
     return this._obj;
   }
 
-  /************************/
-  /* remove item by key   */
-  /************************/
-  public remove(key: string) {
+  /**
+   * Removes an item from sessionStorage by the specified key.
+   *
+   * This method deletes the entry in sessionStorage associated with the provided key.
+   *
+   * @param {any} key - The key of the item to be removed from sessionStorage.
+   *
+   * @returns {void} - No return value.
+   */
+  public remove(key: string): void {
     localStorage.removeItem(key);
   }
 
