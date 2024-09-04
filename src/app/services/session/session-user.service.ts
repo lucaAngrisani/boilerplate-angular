b5edb8f1cf18231272a8553c97831ce5d39559b8
Luca Angrisani <angrisani.luca@gmail.com>
2024-09-04 12:27:14 +0200

    update ng 18.2.2 start

--- src/app/services/session/session-user.service.ts
+++ src/app/services/session/session-user.service.ts
@@ -1,5 +1,4 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import { Injectable, WritableSignal, signal } from "@angular/core";
 import { User } from "src/app/models/user.model";
 import { SESSION_KEY } from "../../shared/session.key";
 import { SessionStorageService } from "./session-storage.service";
@@ -8,55 +7,34 @@ import { SessionStorageService } from "./session-storage.service";
   providedIn: "root",
 })
 export class SessionUserService {
-  public userObservable: BehaviorSubject<User> = new BehaviorSubject<User>(null);
+  public userLogged: WritableSignal<User | null> = signal(null);
 
-  /** YOU CAN USE BOTH SessionStorageService AND LocalStorageService */
+  /** YOU CAN USE BOTH SessionStorageService AND/OR LocalStorageService */
   constructor(
     private sss: SessionStorageService,
   ) {
-    this.userObservable.next(this.getCurrentUser());
+    this.setCurrentUser(this.getCurrentUser());
   }
 
   public setCurrentUser(_user: User) {
-    this.userObservable.next(_user);
+    this.userLogged.set(_user);
 
     this.sss.remove(SESSION_KEY.KEY_USER);
     this.sss.save(SESSION_KEY.KEY_USER, _user.toSession());
   }
 
-  public setLoginSucceded(_isSucceded: boolean) {
-    this.sss.save("isLoginSucceeded", _isSucceded);
-  }
-
-  public getLoginSucceded() {
-    return this.sss.load("isLoginSucceeded");
-  }
-
   public getCurrentUser(): User {
-    let user = this.sss.load(SESSION_KEY.KEY_USER);
+    let user = this.sss.load<User>(SESSION_KEY.KEY_USER);
     return user && new User(user);
   }
 
   public removeUser() {
-    this.userObservable.next(null);
+    this.userLogged.set(null);
     return this.sss.remove(SESSION_KEY.KEY_USER);
   }
 
   public isLoggedUser() {
     return this.sss.load(SESSION_KEY.KEY_USER) != null;
-  }
-
-  public getObservableUser(): Observable<User> {
-    if (this.isLoggedUser()) {
-      this.fetchUser();
-    }
-    return this.userObservable.asObservable();
-  }
-
-  public fetchUser(): void {
-    let user: User = this.getCurrentUser();
-    // user should contains roles has been granted
-    this.userObservable.next(user);
   }
 
   public clear() {
--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,3 +1,2 @@
-
 export const LANG_STORAGE_NAME = 'lang';
 export const APP_STATE = 'APP_STATE';
